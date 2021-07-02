a3d7551e60d05732cfd154415a3f09c641169929
Hùng Trần <tranphihung312@gmail.com>
2021-07-03 02:26:38 +0700

    fix scollbar

--- components/Layout.js
+++ components/Layout.js
@@ -7,18 +7,22 @@ import Style from './RequestRow.module.css'
 
 export default props => {
     return (
-        <div  className={Style.bgclor} style={{overflow: "hidden"}}>
+        <div className={Style.bgclor} style={{ overflow: "hidden" }}>
             {/* <div style={{}}> */}
-                   <Container style={{marginTop: 80}} >
-            <Head>
-                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-            </Head>
-            <Header />
-            {props.children}
-        </Container>
-            </div>
-         
+            <Container style={{
+                marginTop: 80, overflow: "auto",
+                height: "calc(100vh - 70px)",
+                padding: "0px 10px"
+            }} >
+                <Head>
+                    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                </Head>
+                <Header />
+                {props.children}
+            </Container>
+        </div>
+
         // </div>
-        
+
     );
 };
